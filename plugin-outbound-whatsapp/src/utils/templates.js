88c1ab61258ed05c8d28e014f548b86ec648e252
Gus Varallo <gustavo@varallo.me>
2022-06-15 23:17:21 -0300

    feat: add templates for example

---
+++ plugin-outbound-whatsapp/src/utils/templates.js
@@ -0,0 +1,4 @@
+export const templates = [
+  'Thank you for purchasing Saul Goodman! We value your feedback and would like to learn more about your experience.',
+  'Hi Saul Goodman, were we able to solve the issue that you were facing?'
+]
